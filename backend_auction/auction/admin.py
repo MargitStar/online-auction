c8cdfb49e5b22c3a99deccaf97b40f91513411d1
MargitStar <sovenokbymargo@mail.ru>
2021-04-27 15:51:24 +0300

    Add Auction and Log models

--- backend_auction/auction/admin.py
+++ backend_auction/auction/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Auction, Lot, Dutch, English
+
+admin.site.register(Lot)
+admin.site.register(Auction)
+admin.site.register(Dutch)
+admin.site.register(English)
--- backend_auction/auction/models.py
+++ backend_auction/auction/models.py
@@ -1,16 +1,8 @@
-from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
 
 from item.models import Item
-
-
-class Lot(models.Model):
-    item = models.OneToOneField(
-        Item,
-        on_delete=models.CASCADE,
-        primary_key=True,
-    )
 
 
 class Auction(models.Model):
@@ -42,13 +34,54 @@ class Auction(models.Model):
         choices=STATUS,
     )
 
-    auction_type_object_id = models.IntegerField()
-    auction_type_content_type = models.ForeignKey(
+    object_id = models.IntegerField()
+    content_type = models.ForeignKey(
         ContentType,
         on_delete=models.PROTECT,
     )
 
-    auction_type = GenericForeignKey(
-        'auction_type_content_type',
-        'auction_type_object_id',
+    auction_type = GenericForeignKey('content_type', 'object_id')
+
+
+class English(models.Model):
+    buy_it_now = models.DecimalField(
+        'opening price, $',
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    reverse_price = models.DecimalField(
+        'opening price, $',
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    auction = GenericRelation(Auction)
+
+
+class Dutch(models.Model):
+    end_price = models.DecimalField(
+        'end price, $',
+        max_digits=10,
+        decimal_places=2,
+    )
+
+    frequency = models.DateTimeField(
+        'frequency',
+    )
+
+    auction = GenericRelation(Auction)
+
+
+class Lot(models.Model):
+    item = models.OneToOneField(
+        Item,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+
+    auction = models.OneToOneField(
+        Auction,
+        on_delete= models.CASCADE,
+
     )
