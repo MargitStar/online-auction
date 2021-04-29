0f17deed7f4f20ccc19ca812958bf4f6c51be47b
Margarita Bobkova <56334517+MargitStar@users.noreply.github.com>
2021-04-29 16:17:33 +0300

    Add Lot inline (#6)
    * Refactor
    
    * Add Lot inline

--- backend_auction/auction/admin.py
+++ backend_auction/auction/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from .models import Auction, Dutch, English, Lot
 
+
+class LotInLine(admin.StackedInline):
+    model = Lot
+
+
+class AuctionAdmin(admin.ModelAdmin):
+    inlines = (LotInLine,)
+
+
 admin.site.register(Lot)
-admin.site.register(Auction)
+admin.site.register(Auction, AuctionAdmin)
 admin.site.register(Dutch)
 admin.site.register(English)
