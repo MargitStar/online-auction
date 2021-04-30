4856b22a9d9c28a2f10e02cc67ba3dc57f773ae6
Margarita Bobkova <56334517+MargitStar@users.noreply.github.com>
2021-04-30 13:16:08 +0300

    Add ordering by current price and closing date (#9)
    * Add ordering by current price and closing date
    
    * Add auction opening date to ordering

--- backend_auction/auction/views.py
+++ backend_auction/auction/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, filters
 
 from auction.serializers import LotSerializer
 from auction.models import Lot
@@ -7,3 +7,5 @@ from auction.models import Lot
 class LotViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = Lot.objects.all()
     serializer_class = LotSerializer
+    filter_backends = [filters.OrderingFilter]
+    ordering_fields = ['auction__current_price', 'auction__opening_date', 'auction__closing_date']
